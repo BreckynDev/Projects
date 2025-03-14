1dbd51c76e55b00434328e5d08095e2f6c542828
BIBS <breckynburke@gmail.com>
2025-03-14 14:30:04 -0600

    Update main.cpp

--- C++/Banking_Records/main.cpp
+++ C++/Banking_Records/main.cpp
@@ -9,32 +9,32 @@ int main()
 {
     // Initilized and declared variables used in this program
     const int DATE_ROW = 3;
-    const double MONTHLY_INTREST = (0.014 / 12); //Divided by 12 because there is 12 months in a year
-    char    moreBankRecord, 
-            transactionType;
+    const double MONTHLY_INTREST = (0.014 / 12); // Divided by 12 because there is 12 months in a year
+    char moreBankRecord,
+        transactionType;
 
     // Main loop to process multiple bank records.
     do
     {
         // Initilized and declared variables used in this program (Rests after reading another bank record)
-        int     month, 
-                previousMonth = 0, 
-                count = 0, 
-                depositCount = 0, 
-                withdrawCount = 0, 
-                line = 0;
+        int month,
+            previousMonth = 0,
+            count = 0,
+            depositCount = 0,
+            withdrawCount = 0,
+            line = 0;
 
-        float   transactionAmount, 
-                interestRate,
-                totalInterest, 
-                roundedTotal;
+        float transactionAmount,
+            interestRate,
+            totalInterest,
+            roundedTotal;
 
-        double  accountBalance = 0.00, 
-                depositTotal = 0.00, 
-                withdrawTotal = 0.00;
-        
-        bool    ifOverDrafted = false;
-        bool    running = true;
+        double accountBalance = 0.00,
+               depositTotal = 0.00,
+               withdrawTotal = 0.00;
+
+        bool ifOverDrafted = false;
+        bool running = true;
 
         ifstream inputFile;
         string text, fileName;
@@ -43,7 +43,7 @@ int main()
         do
         {
             cout << "What is the name of the file that holds the bank records? ";
-            cin >> fileName; 
+            cin >> fileName;
 
             inputFile.open(fileName);
 
@@ -72,7 +72,7 @@ int main()
             // Month
             else if (count == 2)
             {
-                //Converts second string into int and stores it in month
+                // Converts second string into int and stores it in month
                 month = stoi(text);
 
                 // Checks if the month has changed if so, applies monthly interest.
@@ -88,13 +88,13 @@ int main()
             // Transation amount
             else if (count == 3)
             {
-                //Converts last string into float and assigning it to transactionAmount
+                // Converts last string into float and assigning it to transactionAmount
                 transactionAmount = stof(text);
 
                 /*
                 If the transtion ammount is negitive then the account is overdrawn
                 else do the transaction
-                */ 
+                */
                 if (accountBalance < 0)
                 {
                     cout << "Error on line: " << line << " of file " << fileName << endl;
@@ -105,7 +105,7 @@ int main()
                 {
                     cout << "Error on line: " << line << " of file " << fileName << endl;
                     cout << "\ttansaction amount cannot be less then 0, quitting program" << endl;
-                    ifOverDrafted = true; //Uses this bool even though its not overdrafted just used to quit
+                    ifOverDrafted = true; // Uses this bool even though its not overdrafted just used to quit
                 }
 
                 else
@@ -131,15 +131,15 @@ int main()
                 */
                 if (count == DATE_ROW)
                     count = 0;
-                    line += 1;
+                line += 1;
             }
         }
 
         if (!ifOverDrafted)
         {
-            cout << "You made " << depositCount << " deposits, for a total deposit of $" 
+            cout << "You made " << depositCount << " deposits, for a total deposit of $"
                  << fixed << setprecision(2) << depositTotal << endl;
-            cout << "You made " << withdrawCount << " withdrawals, for a total withdrawal of $" 
+            cout << "You made " << withdrawCount << " withdrawals, for a total withdrawal of $"
                  << fixed << setprecision(2) << withdrawTotal << endl;
             cout << "Total interest: " << fixed << setprecision(2) << totalInterest << endl;
             cout << "Current Account Balance: $" << fixed << setprecision(2) << accountBalance << endl;
@@ -147,13 +147,13 @@ int main()
 
         inputFile.close(); // Closes the file
 
-        //Check if user would like to contuine reading bank records
+        // Check if user would like to contuine reading bank records
         cout << "Would you like to read another bank record (y/n): ";
         cin >> moreBankRecord;
 
         cout << "\n";
 
-    } while (moreBankRecord == 'y' || moreBankRecord == 'Y' );
-    
+    } while (moreBankRecord == 'y' || moreBankRecord == 'Y');
+
     return 0;
 }
